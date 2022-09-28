3ea3222aaededeaa6b024c9ca3102c28c9b249c6
ellie29903 <eftaylor1999@gmail.com>
2022-09-28 15:08:27 +0100

    created index, create and show methods

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -2,16 +2,34 @@ class VehiclesController < ApplicationController
   # GET /vehicles
   def index
     # TODO: return all vehicles
+    vehicles = Vehicle.order('created_at DESC')
+    render json: {data:vehicles}
   end
 
   # GET /vehicles/1
   def show
     # TODO: return the vehicle specified by the id
+    vehicle = Vehicle.find(params[:id])
+    render json: {data:vehicle}
   end
 
   # POST /vehicles
   def create
     # TODO: create vehicle
+    vehicle = Vehicle.new(vehicle_params)
+
+    if vehicle.save
+      render json: {data:vehicle}
+    else
+      render json: {data:vehicle.errors}
+    end 
+    
+  end
+
+  private
+
+  def vehicle_params
+    params.permit(:make, :model, :year, :battery, :maxrange)
   end
 
   # PATCH/PUT /vehicles/1
