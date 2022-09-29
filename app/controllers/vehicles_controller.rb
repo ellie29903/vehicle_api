7bdb8b3645e47e6a99bed7b1cf83fbd2759358b8
ellie29903 <eftaylor1999@gmail.com>
2022-09-29 10:25:06 +0100

    implemented pagination

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -2,7 +2,7 @@ class VehiclesController < ApplicationController
   # GET /vehicles
   def index
     # TODO: return all vehicles
-    vehicles = Vehicle.order('created_at DESC')
+    vehicles = Vehicle.order('created_at DESC').page(params[:page]).per(params[:per])
     render json: {data:vehicles}
   end
 
